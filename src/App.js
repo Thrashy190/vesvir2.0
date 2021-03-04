bb0ed3bb220c0970a04a7f562481c4384e7a153e
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-03 23:50:00 -0700

    Implemented pose detection and drawing utilities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
+import React, { useRef } from 'react';
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as posenet from '@tensorflow-models/posenet';
 import Webcam from 'react-webcam';
+import { drawKeypoints, drawSkeleton } from './utilities';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Loas posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.5,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext('2d');
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose['keypoints'], 0.5, ctx);
+    drawSkeleton(pose['keypoints'], 0.5, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam className="Webcam" />
-        <canvas className="Canvas" />
+        <Webcam ref={webcamRef} className="Webcam" />
+        <canvas ref={canvasRef} className="Canvas" />
       </header>
     </div>
   );
